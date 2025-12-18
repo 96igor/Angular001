e4437b246df0e472c27c08318b2add0ba294eb9a
Игорь Царёв <tsarov88@gmail.com>
2025-12-18 16:06:11 +0200

    basics of typescript for angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Note } from './models/note.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('itproger');
+  protected readonly notes = signal<Note[]>([
+    {
+    id: 1,
+    title: 'Первая заметка',
+    content: 'Привет, это наша первая заметка',
+    createdAt: new Date()
+    },
+    {
+    id: 2,
+    title: 'Вторая заметка',
+    content: 'Angular - это удобно!',
+    createdAt: new Date()
+    }
+  ])
 }
