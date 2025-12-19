b6f01c93aabbdad509136957ccb683c9b8c9000b
Игорь Царёв <tsarov88@gmail.com>
2025-12-19 13:14:42 +0200

    adding components to the project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { Note } from './models/note.model';
 import { CommonModule } from '@angular/common';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  handleSubscribe() {
+    console.log("all is working");
+    alert("all is working");
+  }
+
   protected readonly notes = signal<Note[]>([
     {
     id: 1,
